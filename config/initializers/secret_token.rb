8a46006f5c6dc978a8380bd6938f77ec61bb4940
Jonathan Friedman <friedmanj98@gmail.com>
2013-03-12 16:15:23 -0500

    FEATURE: init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nuangel::Application.config.secret_key_base = '5330f692d6b35a16731d6bedde4fdefec52a0ca89f076f534f2ddff329a8ff512dc15354afc3147af67adae36dc0a1a6523e57b1302388f3a2ef5d60899aa1e9'
